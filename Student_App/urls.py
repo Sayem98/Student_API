d123590bd2abdf954cad6849e7b6ab9133e97747
sayem98 <sayemabedin.bd@gmail.com>
2021-08-27 08:48:47 +0600

    Concrete view classes created for student model.

--- Student_App/urls.py
+++ Student_App/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 # from .views import StudentsView
-from .views import StudentView
+from .views import StudentListView, StudentCreateView,\
+    StudentRetrieveView, StudentUpdateView, StudentDestroyView, StudentLCView, StudentRUDView
 
 urlpatterns = [
-    path('studentapi/', StudentView.as_view()),
-    path('studentapi/<int:pk>', StudentView.as_view())
+    # path('studentapi/', StudentListView.as_view()),
+    # path('studentapi/', StudentCreateView.as_view()),
+    path('studentapi/', StudentLCView.as_view()),
+    # path('studentapi/<int:pk>/', StudentRetrieveView.as_view()),
+    # path('studentapi/<int:pk>/', StudentUpdateView.as_view()),
+    # path('studentapi/<int:pk>/', StudentDestroyView.as_view()),
+    path('studentapi/<int:pk>/', StudentRUDView.as_view()),
+
 ]
--- Student_App/views.py
+++ Student_App/views.py
@@ -6,6 +6,53 @@ from rest_framework.views import APIView
 from .models import StudentModel
 from .serializers import StudentSerializer
 
+from rest_framework.generics import ListAPIView, CreateAPIView, \
+    RetrieveAPIView, UpdateAPIView, DestroyAPIView, ListCreateAPIView, RetrieveUpdateAPIView,\
+    RetrieveDestroyAPIView, RetrieveUpdateDestroyAPIView
+
+
 # Create your views here.
 
 # ===== Concrete api views =======
+
+class StudentListView(ListAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentCreateView(CreateAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentRetrieveView(RetrieveAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentUpdateView(UpdateAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentDestroyView(DestroyAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+
+# ===== combine concrete api view =======
+
+
+class StudentLCView(ListCreateAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+
+# class StudentRUView(RetrieveUpdateAPIView):
+#     queryset = StudentModel.objects.all()
+#     serializer_class = StudentSerializer
+
+
+class StudentRUDView(RetrieveUpdateDestroyAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
