4653c0ffe8b15569846ae9b23b6b9de5fc68a491
sayem98 <sayemabedin.bd@gmail.com>
2021-08-30 20:05:52 +0600

    Token Authentications. Customauthtoken and obtain_auth_token

--- Student_App/urls.py
+++ Student_App/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 # from .views import StudentsView
-from .views import StudentViewSet
+from .views import StudentViewSet, CustomAuthToken
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 # creating router object.
 router = DefaultRouter()
@@ -15,5 +16,6 @@ router.register('studentapi', StudentViewSet, basename='student')
 urlpatterns = [
     path('', include(router.urls)),
     # adds login and logout system in our api.
-    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('gettoken/', CustomAuthToken.as_view())
 ]
--- Student_App/views.py
+++ Student_App/views.py
@@ -9,9 +9,11 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, \
     RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser,\
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, \
     IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 
 # Create your views here.
@@ -19,9 +21,16 @@ from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser,\
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = StudentModel.objects.all()
     serializer_class = StudentSerializer
-    # authentication_classes = [BasicAuthentication]
-    authentication_classes = [SessionAuthentication]
-    # permission_classes = [IsAuthenticated]
+    # =======Authentications=====
+    # authentication_classes = [BasicAuthentication] # Never use in deployment.
+    # authentication_classes = [SessionAuthentication]
+    authentication_classes = [TokenAuthentication]
+    # by admin panel token generate.
+    # by cmd ---python manage.py drf_create_token user_1
+    # by exposing an api endpoint By user.
+
+    # ====== Permissions=========
+    permission_classes = [IsAuthenticated]
     # permission_classes = [AllowAny]
     # when is staff is true.
     # permission_classes = [IsAdminUser]
@@ -29,4 +38,19 @@ class StudentViewSet(viewsets.ModelViewSet):
     # for custom permissions from Admin site.
     # permission_classes = [DjangoModelPermissions]
     # same as django model permissions but unauthenticated user have view only.
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    # permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
