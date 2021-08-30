361629242d1318ec429a1259ce0a92f386f23446
sayem98 <sayemabedin.bd@gmail.com>
2021-08-30 20:22:15 +0600

    Creating token when user is created by signals-post_save in User Model

--- Student_App/models.py
+++ Student_App/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 
 # Create your models here.
 
@@ -7,3 +12,10 @@ class StudentModel(models.Model):
     name = models.CharField(max_length=50)
     roll = models.IntegerField()
     city = models.CharField(max_length=50)
+
+
+# Signal for creating token when user is created.
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- Student_App/urls.py
+++ Student_App/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 # from .views import StudentsView
-from .views import StudentViewSet, CustomAuthToken
+from .views import StudentViewSet # CustomAuthToken
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -17,5 +17,5 @@ urlpatterns = [
     path('', include(router.urls)),
     # adds login and logout system in our api.
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('gettoken/', CustomAuthToken.as_view())
+    # path('gettoken/', CustomAuthToken.as_view())
 ]
